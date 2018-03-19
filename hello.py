4ab51d4bc640ec82bfebb64541b82585a20e2f65
Mengzouzoe <mengzou_zoe@icloud.com>
2018-03-19 14:33:08 +0800

    Adding login.html

--- hello.py
+++ hello.py
@@ -2,20 +2,47 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootsrap = Bootstrap(app)
 moment = Moment(app)
 
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=5)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+class RegisterForm(FlaskForm):
+    username = StringField('username',validators=[InputRequired(), Length(min=4, max=5)])
+    email = StringField('email', validators=[InputRequired(), Email(message='Invaild message'), Length(max=50)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    
 @app.route('/')
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' +form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+    
+    return render_template('login.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
